bc6c634ee73a59891a5601e596cb372f614c99dc
Asif2501 <asifshah4495@gmail.com>
2026-08-18 17:27:17 +0530

    add vercel file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDGB9tsMWcng-2LY0rCqAIcQ4Jp_nIeLyE",
-  authDomain: "neural-ripple-504906-f6.firebaseapp.com",
+  authDomain: "auth.tncomputers.in",
   projectId: "neural-ripple-504906-f6",
   storageBucket: "neural-ripple-504906-f6.firebasestorage.app",
   messagingSenderId: "537746754440",
